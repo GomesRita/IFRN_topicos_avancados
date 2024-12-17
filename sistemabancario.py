239e09a401b7374fe8e8951d24d4c79188e3660f
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2024-12-16 21:21:13 -0300

    Create sistemabancario.py
    Subindo arquivo principal

---
+++ sistemabancario.py
@@ -0,0 +1,15 @@
+class SistemaBancario:
+    def cadastrar_conta(self):
+        pass
+
+    def consultar_saldo(self):
+        pass
+
+    def creditar(self):
+        pass
+
+    def debitar(self):
+        pass
+
+    def transferir(self):
+        pass
