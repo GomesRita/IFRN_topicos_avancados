00c8cd6aae91d63752a1a56ca01dc92c32551a23
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2024-12-17 19:17:40 +0000

    realizando testes e pequenas correções na estrutura de arquivos

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,9 @@
-from sistema_bancario import SistemaBancario
+import sys
+import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from backend.sistema_bancario import SistemaBancario
 sistema = SistemaBancario()
 
 def menu():
