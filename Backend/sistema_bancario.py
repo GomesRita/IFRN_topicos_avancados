5cb59799dcb7f4e4c2faa9c8b33d9f0b832b288a
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2024-12-27 17:58:01 -0300

    Corrigindo detalhe identacao sistema_bancario.py

--- Backend/sistema_bancario.py
+++ Backend/sistema_bancario.py
@@ -14,7 +14,7 @@ class SistemaBancario:
             self.contas[numero] = Conta(numero)
             print(f"Conta {numero} cadastrada com sucesso.")
 
-def realizar_credito(self, numero, valor):
+   def realizar_credito(self, numero, valor):
         conta = self.contas.get(numero)
         if conta:
             conta.saldo += valor
